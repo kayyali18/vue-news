f84c2193bca95f03a2907eab2a44c0562c99f614
Ahmad Kayyali <kayyali18@gmail.com>
2019-04-28 15:09:12 -0400

    :sparkles: Succesfully add users to firestore

--- plugins/firestore.js
+++ plugins/firestore.js
@@ -2,22 +2,21 @@ import firebase from 'firebase/app'
 import 'firebase/firestore'
 
 // Initialize Firebase
-if (!firebase.app.length) {
-  var config = {
-    apiKey: 'AIzaSyD_-MD5xNRniFYwX-ofogvRZViz7XJQXoI',
-    authDomain: 'the-thinker-news.firebaseapp.com',
-    databaseURL: 'https://the-thinker-news.firebaseio.com',
-    projectId: 'the-thinker-news',
-    storageBucket: 'the-thinker-news.appspot.com',
-    messagingSenderId: '122532747384'
-  }
 
-  // Init firebase
-  firebase.initializeApp(config)
-
-  // Init settings and timestamp by default
-  firebase.firestore().settings({})
+var config = {
+  apiKey: 'AIzaSyD_-MD5xNRniFYwX-ofogvRZViz7XJQXoI',
+  authDomain: 'the-thinker-news.firebaseapp.com',
+  databaseURL: 'https://the-thinker-news.firebaseio.com',
+  projectId: 'the-thinker-news',
+  storageBucket: 'the-thinker-news.appspot.com',
+  messagingSenderId: '122532747384'
 }
+
+// Init firebase
+firebase.initializeApp(config)
+
+// Init settings and timestamp by default
+firebase.firestore().settings({})
 
 // create reference to firestore module
 const db = firebase.firestore()
