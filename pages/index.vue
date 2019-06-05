f70f630cf6b9666286575c403c0c9393b4f2d37e
Ahmad Kayyali <kayyali18@gmail.com>
2019-06-04 22:36:56 -0400

    :sparkles: Feed has empty state, fixes load feed issue

--- pages/index.vue
+++ pages/index.vue
@@ -47,9 +47,15 @@
         </md-select>
       </md-field>
 
+      <!-- Default Markup (if Feed Empty) -->
+      <md-empty-state class="md-primary" v-if="feed.length === 0 && !user" md-icon="bookmarks" md-label="Nothing in Feed" md-description="Login to bookmark headlines">
+        <md-button @click="$router.push('/login')" class="md-primary md-raised">Login</md-button>
+      </md-empty-state>
 
-      <!-- Feed Content -->
-      <md-list class="md-triple-line" v-for="headline in feed" :key="headline.id">
+    <md-empty-state v-else-if="feed.length === 0" class="md-accent" md-icon="bookmark_outline" md-label="Nothing in Feed" md-description="Anything you bookmark will be safely stored here"></md-empty-state>
+
+      <!-- Feed Content (if Feed Not Empty) -->
+      <md-list class="md-triple-line" v-else v-for="headline in feed" :key="headline.id">
         <md-list-item>
           <md-avatar><img :src="headline.urlToImage" :alt="headline.title"></md-avatar>
 
